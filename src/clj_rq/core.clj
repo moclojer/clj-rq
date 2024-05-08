109d75c8ef207c13afb30211461b94a425ef9d74
Avelino <31996+avelino@users.noreply.github.com>
2024-05-08 01:56:55 +0000

    redis/client initial struct
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/clj_rq/core.clj
+++ src/clj_rq/core.clj
@@ -1,1 +1,22 @@
-(ns clj-rq.core)
+(ns clj-rq.core
+  (:import redis.clients.jedis JedisPool))
+
+;; redis connection pool to be thread safe
+(def ^{:private true} *redis-pool* (ref nil))
+
+(defn redis-client
+  "redis connect client"
+  [url]
+  (dosync
+    (ref-set *redis-pool* (JedisPool. url)))
+  (let [redis (.getResource @*redis-pool*)]
+    (.select redis 0)
+    redis))
+
+(defn redis-client-kill
+  []
+  (.destroy @*redis-pool*))
+
+(defn redis-client-disconnect
+  []
+  (.returnResource @*redis-pool*))
