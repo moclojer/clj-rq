eac5bbabd12e804a4997e70f899eb67665fe87dd
Avelino <31996+avelino@users.noreply.github.com>
2024-05-09 17:18:54 -0300

    pubsub: initial publish
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/com/moclojer/rq/pubsub.clj
+++ src/com/moclojer/rq/pubsub.clj
@@ -1,1 +1,22 @@
-(ns com.moclojer.rq.pubsub)
+(ns com.moclojer.rq.pubsub
+  (:import [redis.clients.jedis JedisPubSub]))
+
+;; (pubsub/publish redis-client "name-subs" "value set")
+(defn publish
+  "Publish a message to a channel"
+  [redis-client channel message]
+  (.publish redis-client channel message))
+
+(defn listener [callback]
+  (try
+    (proxy [JedisPubSub] []
+      (onMessage [channel message]
+        (callback channel message)))
+    (catch Exception e
+      (ex-message e) nil)))
+
+;; (pubsub/subscribe redis-client ["name-subs"])
+(defn subscribe
+  "Subscribe to a channel"
+  [redis-client channels]
+  (.subscribe (listener redis-client) channels))
