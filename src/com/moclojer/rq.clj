1ad4f36e1fbd1db8ea88964176c380886719b338
Avelino <31996+avelino@users.noreply.github.com>
2024-05-26 12:36:54 -0300

    rq/main: rename client connect, only client
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/com/moclojer/rq.clj
+++ src/com/moclojer/rq.clj
@@ -9,7 +9,7 @@
   ^{:private true :dynamic true}
   *redis-pool* (ref nil))
 
-(defn redis-client
+(defn client
   "redis connect client"
   [url]
   (when-not @*redis-pool*
@@ -17,18 +17,27 @@
      (ref-set *redis-pool* (JedisPooled. url))))
   *redis-pool*)
 
-(defn redis-client-kill
+(defn client-kill
   "redis kill client"
   []
   (.destroy @*redis-pool*))
 
-(defn redis-client-disconnect
+(defn client-disconnect
   "redis disconnect client"
   []
   (.returnResource @*redis-pool*))
 
+(defn -main
+  [& _]
+  (client "redis://localhost:6379")
+  (queue/producer *redis-pool* "my-queue" {:now (java.time.LocalDateTime/now)
+                                           :foo "bar"})
+  (println :size (queue/consumer-size *redis-pool* "my-queue"))
+  (queue/consumer *redis-pool* "my-queue" #(prn :msg %1)))
+
+
 (comment
-  (redis-client "redis://localhost:6379")
+  (client "redis://localhost:6379")
   (queue/producer *redis-pool* "my-queue" {:now (java.time.LocalDateTime/now)
                                            :foo "bar"})
   (println :size (queue/consumer-size *redis-pool* "my-queue"))
