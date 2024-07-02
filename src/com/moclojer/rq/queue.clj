52d6f9e8150186e5c19c7c71d67ed740de9b7fb4
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-02 10:18:27 -0300

    refactor: improve `queue.clj`

--- src/com/moclojer/rq/queue.clj
+++ src/com/moclojer/rq/queue.clj
@@ -1,48 +1,118 @@
-(ns com.moclojer.rq.queue)
+(ns com.moclojer.rq.queue
+  (:require
+   [clojure.core.async :as async]
+   [clojure.edn :as edn]
+   [com.moclojer.rq.utils :as utils]))
 
-(defn pattern-name
-  "Return the pattern name for the queue, which is the name prefixed with `rq:`"
-  [name]
-  (str "rq:" name))
+(defn push!
+  [client queue-name message & options]
+  ;; NOTE: maybe use clojure.tools.logging? this way the enduser chooses which
+  ;; logging mechanism to use by themselves.
+  (let [{:keys [direction pattern _at _in _retry _retry-delay]
+         :or {direction :l
+              pattern :rq}} options
+        packed-queue-name (utils/pack-pattern pattern queue-name)
+        encoded-message (into-array [(pr-str message)])]
+    (if (= direction :l)
+      (.lpush @client packed-queue-name encoded-message)
+      (.rpush @client packed-queue-name encoded-message))))
 
-;; queue:push
-;; (queue/producer client queue-name {:foo "bar"} :at ... :in ... :retry 3 :retry-delay 10)
-(defn producer
-  "Push a job to the queue"
-  [redis-client queue-name message & {:keys [direction at in retry retry-delay]
-                                      :or {direction "l" retry 3 retry-delay 10}}]
-  (println at in retry retry-delay)
-  (let [qname (pattern-name queue-name)
-        msg (into-array [(pr-str message)])]
-    (if (= direction "r")
-      (.rpush @redis-client qname msg)
-      (.lpush @redis-client qname msg))))
+(comment
+  (import redis.clients.jedis.JedisPooled)
 
-(defn consumer-size
-  "get size of the queue"
-  [redis-client queue-name]
-  (.llen @redis-client (pattern-name queue-name)))
+  (let [client (atom (JedisPooled. "redis://localhost:6379"))]
+    (push! client "my-queue" {:hello true} :direction :l)
+    (.close @client))
+  ;;
+  )
 
-;; queue:process and pop
-;; (queue/consumer client queue-name (fn [job] (println job))
-(defn consumer
-  "Consume a message from the queue, blocking in loop until a message is available."
-  [redis-client queue-name consumer-fn & {:keys [size loop-sleep direction]
-                                          :or {direction "l" size -1 loop-sleep 0}}]
-  (let [qname (pattern-name queue-name)]
-    (loop []
-      (Thread/sleep loop-sleep)
-      (flush)
-      (let [msg (if (= direction "r")
-                  (.rrange @redis-client qname 0 size)
-                  (.lrange @redis-client qname 0 size))]
-        (when msg
-          (consumer-fn msg)
-          (println :size (consumer-size redis-client qname))
-          (if (= direction "r")
-            (.rpop @redis-client qname)
-            (.lpop @redis-client qname))
-          (System/exit 0)
-          (recur))))))
+(defn pop!
+  [client queue-name & options]
+  (let [{:keys [direction pattern]
+         :or {direction :l
+              pattern :rq}} options
+        packed-queue-name (utils/pack-pattern pattern queue-name)
+        message (if (= direction :l)
+                  (.lpop @client packed-queue-name)
+                  (.rpop @client packed-queue-name))]
+    (edn/read-string message)))
 
+(comment
+  (import redis.clients.jedis.JedisPooled)
 
+  (let [client (atom (JedisPooled. "redis://localhost:6379"))]
+    (push! client "my-queue" {:hello true} :direction :r)
+    (let [popped-val (pop! client "my-queue" :direction :r)]
+      (.close @client)
+      popped-val))
+  ;; => {:hello true}
+  )
+
+(defn llen
+  "get size of a queue"
+  [client queue-name & options]
+  (let [{:keys [pattern]
+         :or {pattern :rq}} options]
+    (.llen @client (utils/pack-pattern pattern queue-name))))
+
+(comment
+  (import redis.clients.jedis.JedisPooled)
+
+  (let [client (atom (JedisPooled. "redis://localhost:6379"))]
+    (push! client "my-queue" {:hello 1} :direction :r)
+    (push! client "my-queue" {:hello 2} :direction :r)
+    (push! client "my-queue" {:hello 3} :direction :r)
+    (let [queue-length (llen client "my-queue")]
+      (pop! client "my-queue" :direction :r)
+      (pop! client "my-queue" :direction :r)
+      (pop! client "my-queue" :direction :r)
+      (.close @client)
+      queue-length))
+  ;; => 3
+  )
+
+(defn wait-and-consume!
+  "returns the channel, which can be closed with async/close!"
+  [client queue-name consume-fn & options]
+  (let [{:keys [direction pattern sleep-time buffer-size]
+         :or {direction  :l
+              pattern    :rq
+              sleep-time 200
+              buffer-size 1024}} options
+        chan (async/chan (async/sliding-buffer buffer-size))]
+
+    ;; wait and retrieve messages
+    (async/go-loop []
+      (when (.isConnected @client)
+        (if-let [?message (pop! client queue-name
+                                :direction direction
+                                :pattern pattern)]
+          (do
+            (Thread/sleep sleep-time)
+            (when (async/>! chan ?message)
+              (recur)))
+          (recur))))
+
+    ;; now consume them
+    (async/go-loop []
+      (when-let [?message (async/<! chan)]
+        (consume-fn ?message)
+        (recur)))
+
+    chan))
+
+(comment
+  (import redis.clients.jedis.JedisPooled)
+
+  (let [client (atom (JedisPooled. "redis://localhost:6379"))
+        chan (wait-and-consume! client "my-queue" #(prn :new-message %))]
+    (Thread/sleep 1000)
+    (push! client "my-queue" {:hello 1})
+    (Thread/sleep 250)
+    (push! client "my-queue" {:hello 2})
+    (Thread/sleep 2000)
+    (push! client "my-queue" {:hello 3})
+    (.close! chan)
+    (.close @client))
+  ;;
+  )
