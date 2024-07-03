912c46bb7c40fe18a5181dc51fe09e54467220a6
Josué Teodoro Moreira <teodoro.josue@protonmail.ch>
2024-07-03 15:19:41 -0300

    style: grammar & log improvements
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/com/moclojer/rq/queue.clj
+++ src/com/moclojer/rq/queue.clj
@@ -5,7 +5,15 @@
    [com.moclojer.rq.utils :as utils]))
 
 (defn push!
-  "Push a message into a queue"
+  "Push a message into a queue.
+  
+  Parameters:
+  - client: Redis client
+  - queue-name: Name of the queue
+  - message: Message to be pushed
+  - options: Optional parameters, including:
+    - direction: Direction to push the message (:l or :r)
+    - pattern: Pattern for the queue name"
   [client queue-name message & options]
   (let [{:keys [direction pattern _at _in _retry _retry-delay]
          :or {direction :l
@@ -18,7 +26,8 @@
                        (.rpush @client packed-queue-name encoded-message))]
 
     (log/debug "pushed to queue"
-               {:queue-name packed-queue-name
+               {:client client
+                :queue-name packed-queue-name
                 :message message
                 :options opts
                 :pushed-count pushed-count})
@@ -26,7 +35,14 @@
     pushed-count))
 
 (defn pop!
-  "Pop a message from a queue"
+  "Pop a message from a queue.
+  
+  Parameters:
+  - client: Redis client
+  - queue-name: Name of the queue
+  - options: Optional parameters, including:
+    - direction: Direction to pop the message (:l or :r)
+    - pattern: Pattern for the queue name"
   [client queue-name & options]
   (let [{:keys [direction pattern]
          :or {direction :l
@@ -38,14 +54,21 @@
                   (.rpop @client packed-queue-name))]
 
     (log/debug "popped from queue"
-               {:queue-name packed-queue-name
+               {:client client
+                :queue-name packed-queue-name
                 :options opts
                 :message message})
 
     (edn/read-string message)))
 
 (defn llen
-  "get size of a queue"
+  "Get the size of a queue.
+
+  Parameters:
+  - client: Redis client
+  - queue-name: Name of the queue
+  - options: Optional parameters, including:
+    - pattern: Pattern for the queue name"
   [client queue-name & options]
   (let [{:keys [pattern]
          :or {pattern :rq}} options]
