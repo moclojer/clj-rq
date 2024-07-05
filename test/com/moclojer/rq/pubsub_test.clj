0b3b89da8c0d5f0456527706a576cb6533075261
J0sueTM <teodoro.josue@protonmail.ch>
2024-07-05 17:06:48 -0300

    test: update `pub/sub` tests accordingly

--- test/com/moclojer/rq/pubsub_test.clj
+++ test/com/moclojer/rq/pubsub_test.clj
@@ -5,23 +5,38 @@
    [com.moclojer.rq.pubsub :as rq-pubsub]
    [com.moclojer.test-utils :as utils]))
 
-(t/deftest pubsub-test
-  (let [client (rq/create-client "redis://localhost:6379")
-        subs (atom [])
-        channels (into [] (repeatedly 20 #(str (random-uuid))))
-        messages (into [] (repeatedly 20 utils/gen-message))
-        chan-msg (zipmap channels messages)]
-    (rq-pubsub/subscribe! client
-                          #(swap! subs conj {:channel %1 :message %2})
-                          channels)
-    (doseq [[channel message] chan-msg]
-      (rq-pubsub/publish! client channel message))
+(defn build-workers
+  [qtt state]
+  (let [channels (repeatedly qtt #(str (random-uuid)))
+        messages (repeatedly qtt utils/gen-message)
+        chans-msgs (zipmap channels messages)]
+    {:chans-msgs chans-msgs
+     :msgs messages
+     :workers (map (fn [[chan msg]]
+                     {:channel chan
+                      :handler #(swap! state conj msg)})
+                   chans-msgs)}))
 
-    (future
-      ;; more than enough time for 20 messages to be consumed
-      (Thread/sleep 3000)
-      (t/is (= (map (fn [[chan msg]]
-                      {:channel chan
-                       :message msg})
-                    chan-msg)
-               @subs)))))
+(t/deftest pubsub-test
+  (let [client (rq/create-client "redis://localhost:6379")]
+
+    (t/testing "archiving/unarchiving"
+      (let [channel (str (random-uuid))
+            message (utils/gen-message)]
+        (rq-pubsub/publish! client channel message)
+        (Thread/sleep 500)
+        (t/is
+         message
+         (rq-pubsub/unarquive-channel! client channel identity))))
+
+    (t/testing "multi pub/sub"
+      (let [state (atom [])
+            {:keys [chans-msgs messages workers]} (build-workers 1 state)]
+        (rq-pubsub/subscribe! client workers)
+        (Thread/sleep 500)
+        (doseq [[chan msg] chans-msgs]
+          (rq-pubsub/publish! client chan msg))
+        (Thread/sleep 1000)
+        (t/is @state messages)))
+
+    (rq/close-client client)))
