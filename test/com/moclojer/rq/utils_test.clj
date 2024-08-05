e236287de5fa27c10b17da3178b979f2aab88e8a
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-08-05 15:29:56 -0300

    doc: improves in-code documentation
    add:
    - testing fucntions for better reviewing
    - TODO: readme.md for better descriptions

--- test/com/moclojer/rq/utils_test.clj
+++ test/com/moclojer/rq/utils_test.clj
@@ -4,12 +4,14 @@
    [com.moclojer.rq.utils :as utils]))
 
 (t/deftest pattern->str-test
+  (t/testing "packing"
   [(t/is "my-queue" (utils/pack-pattern :none "my-queue"))
    (t/is "rq:my-queue" (utils/pack-pattern :rq "my-queue"))
    (t/is "rq:pubsub:my-queue" (utils/pack-pattern :pubsub "my-queue"))
-   (t/is "rq:pubsub:pending:my-queue" (utils/pack-pattern :pending "my-queue"))]
+   (t/is "rq:pubsub:pending:my-queue" (utils/pack-pattern :pending "my-queue"))])
 
+  (t/testing "unpacking"
   [(t/is "my-queue" (utils/unpack-pattern :none "my-queue"))
    (t/is "my-queue" (utils/unpack-pattern :rq "rq:my-queue"))
    (t/is "my-queue" (utils/unpack-pattern :pubsub "rq:pubsub:my-queue"))
-   (t/is "my-queue" (utils/unpack-pattern :pending "rq:pubsub:pending:my-queue"))])
+   (t/is "my-queue" (utils/unpack-pattern :pending "rq:pubsub:pending:my-queue"))]))
