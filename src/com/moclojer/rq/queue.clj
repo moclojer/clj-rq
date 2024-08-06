d57030c403fa37164113827863d3ee61ffa8a9c0
Felipe-gsilva <felipe.gsilva@protonmail.com>
2024-08-06 19:30:06 -0300

    feat: adds all the commands we need

--- src/com/moclojer/rq/queue.clj
+++ src/com/moclojer/rq/queue.clj
@@ -3,8 +3,7 @@
   (:require
    [clojure.edn :as edn]
    [clojure.tools.logging :as log]
-   [com.moclojer.rq.utils :as utils]
-   [clojure.core.async :as async]))
+   [com.moclojer.rq.utils :as utils]))
 
 (defn push!
   "Push a message into a queue.
@@ -71,26 +70,34 @@
     - pattern: Pattern for the queue name"
   [client queue-name & options]
   (let [{:keys [pattern]
-         :or {pattern :rq}} options]
-    (.llen @client (utils/pack-pattern pattern queue-name))))
+         :or {pattern :rq}} options
+        packed-queue-name (utils/pack-pattern pattern queue-name)]
+    (.llen @client packed-queue-name)))  
 
 (defn bpop!
-  "Block pop a message
-  
+  "Pop a message from a queue. Blocking if necessary.
+
   Parameters:
-  - same as pop!
-    - client: Redis client
-    - queue-name: Name of the queue
-    - options: Optional parameters, including:
-      - pattern: Pattern for the queue name
-  - timeout: Timeout for retrying to pop!"
-  ([client queue-name timeout & options]
-  (or (apply (partial pop! client queue-name) options)
-      (if (>= timeout 1000)
-        (do
-          (Thread/sleep 1000)
-          (bpop! client queue-name (- timeout 1000) options))
-        nil))))
+  - client: Redis client
+  - queue-name: Name of the queue
+  - tmot: Blocking timeout
+  - options:
+    - direction: Direction to pop the message (:l or :r)
+    - pattern: Pattern for the queue name"
+  [client queue-name tmot & {:keys [direction pattern]
+                             :or {direction :l pattern :rq}}]
+  (let [packed-queue-name (utils/pack-pattern pattern queue-name)
+        result (if (= direction :l)
+                 (.blpop @client tmot packed-queue-name)
+                 (.brpop @client tmot packed-queue-name))]
+    (when result
+      (let [message (second result)]
+        (log/debug "popped from queue"
+                   {:client client
+                    :queue-name packed-queue-name
+                    :options {:direction direction :pattern pattern}
+                    :message message})
+        (edn/read-string message)))))
 
 (defn lrange 
   "Return an entire range given min and max indexes
@@ -98,14 +105,13 @@
   Parameters:
   - client: Redis client
   - queue-name: Name of the queue
-  - min: floor index
-  - max: ceiling index"
-  [client queue-name min max & options]
+  - floor: floor index
+  - ceil: ceiling index"
+  [client queue-name floor ceil & options]
   (let [{:keys [pattern]
          :or {pattern :rq}} options
         packed-queue (utils/pack-pattern pattern queue-name)]
-    (.range @client packed-queue min max)))
-
+    (.lrange @client packed-queue floor ceil)))
 
 (defn lindex
   "Return a element in a specified index
@@ -120,7 +126,7 @@
         packed-queue (utils/pack-pattern pattern queue-name)]
     (.lindex @client packed-queue index)))
 
-(defn lset 
+(defn lset
   "Set a new message in the specified index
   
   Parameters:
@@ -128,15 +134,14 @@
   - queue-name: Name of the queue
   - index: specific index to access
   - msg: new msg to be added"
-  [client queue-name index msg & options]
-  ;TODO remove (let [old-msg (lindex client queue-name index)](println old-msg))
-   (let [{:keys [pattern]
-         :or {pattern :rq}} options
-        packed-queue (utils/pack-pattern pattern queue-name)]
-    (.lset @client packed-queue index msg))))
+  [client queue-name index msg & {:keys [pattern]
+                                  :or {pattern :rq}}]
+  (let [packed-queue-name (utils/pack-pattern pattern queue-name)]
+    (.lset @client packed-queue-name index msg)))
+
 
 (defn lrem 
-  "removes a specified occurance (defined by count) of the message given (if any)
+  "Removes a specified occurance of the message given (if any)
   
   Parameters:
   - client: Redis client
@@ -149,16 +154,93 @@
   [client queue-name cnt msg & options]
    (let [{:keys [pattern]
          :or {pattern :rq}} options
-        packed-queue (utils/pack-pattern pattern queue-name)]
-    (.lrem @client packed-queue cnt msg))))
+        packed-queue-name (utils/pack-pattern pattern queue-name)]
+    (.lrem @client packed-queue-name cnt msg)))
 
-(comment 
-  (defn linsert [])
 
-  (defn ltrim [])
+(defn linsert 
+  "Insert a message before the first occurance of a pivot given.
 
-  (defn rpoplpush [])
+  Parameters:
+  - client: Redis client
+  - queue-name: Name of the queue
+  - msg: new msg to be added
+  - pivot: pivot message to be added before or after
+  - options:
+    - pos (keywords):
+      - before: insert the message before the pivot
+      - after: insert the message after the pivot"
+  [client queue-name msg pivot & options]
+  (let [{:keys [pos pattern]
+         :or {pos :before 
+              pattern :rq}} options
+        packed-queue-name (utils/pack-pattern pattern queue-name)]
+    (.linsert @client packed-queue-name pos pivot msg)))
 
-  (defn brpoplpsuh [])
 
-  (defn lmove []))
+(defn ltrim
+  "Trim a list to the specified range.
+
+  Parameters:
+  - client: Redis client
+  - queue-name: Name of the queue
+  - start: start index
+  - stop: stop index
+  - options:
+    - pattern: pattern to pack the queue name"
+  [client queue-name start stop & options]
+  (let [{:keys [pattern]
+         :or {pattern :rq}} options
+        packed-queue-name (utils/pack-pattern pattern queue-name)]
+    (.ltrim @client packed-queue-name start stop)))
+
+(defn rpoplpush
+  "Remove the last element in a list and append it to another list.
+
+  Parameters:
+  - client: Redis client
+  - source-queue: Name of the source queue
+  - destination-queue: Name of the destination queue
+  - options:
+    - pattern: pattern to pack the queue names"
+  [client source-queue destination-queue & options]
+  (let [{:keys [pattern]
+         :or {pattern :rq}} options
+        packed-source-queue (utils/pack-pattern pattern source-queue)
+        packed-destination-queue (utils/pack-pattern pattern destination-queue)]
+    (.rpoplpush @client packed-source-queue packed-destination-queue)))
+
+(defn brpoplpush
+  "Remove the last element in a list and append it to another list, blocking if necessary.
+
+  Parameters:
+  - client: Redis client
+  - source-queue: Name of the source queue
+  - destination-queue: Name of the destination queue
+  - timeout: timeout in seconds
+  - options:
+    - pattern: pattern to pack the queue names"
+  [client source-queue destination-queue timeout & options]
+  (let [{:keys [pattern]
+         :or {pattern :rq}} options
+        packed-source-queue (utils/pack-pattern pattern source-queue)
+        packed-destination-queue (utils/pack-pattern pattern destination-queue)]
+    (.brpoplpush @client packed-source-queue packed-destination-queue timeout)))
+
+(defn lmove
+  "Atomically return and remove the first/last element (head/tail depending on the wherefrom argument) of the source list, and push the element as the first/last element (head/tail depending on the whereto argument) of the destination list.
+
+  Parameters:
+  - client: Redis client
+  - source-queue: Name of the source queue
+  - destination-queue: Name of the destination queue
+  - wherefrom: 'LEFT' or 'RIGHT'
+  - whereto: 'LEFT' or 'RIGHT'
+  - options:
+    - pattern: pattern to pack the queue names"
+  [client source-queue destination-queue wherefrom whereto & options]
+  (let [{:keys [pattern]
+         :or {pattern :rq}} options
+        packed-source-queue (utils/pack-pattern pattern source-queue)
+        packed-destination-queue (utils/pack-pattern pattern destination-queue)]
+    (.lmove @client packed-source-queue packed-destination-queue wherefrom whereto)))
--- test/com/moclojer/rq/queue_test.clj
+++ test/com/moclojer/rq/queue_test.clj
@@ -8,13 +8,15 @@
 (t/deftest queue-test
   (let [client (rq/create-client "redis://localhost:6379")
         queue-name (str (random-uuid))
-        message (utils/gen-message)]
+        another-queue-name (str (random-uuid))
+        message (utils/gen-message)
+        another-message (utils/gen-message)]
 
     (t/testing "raw"
       (rq-queue/push! client queue-name message)
-      (rq-queue/push! client queue-name (utils/gen-message))
+      (rq-queue/push! client queue-name another-message)
       (t/is (= 2 (rq-queue/llen client queue-name)))
-      (t/is (= message (rq-queue/pop! client queue-name :direction :r))))
+      (t/is (= message (rq-queue/pop! client queue-name {:direction :r}))))
 
     (t/testing "direction"
       (rq-queue/push! client queue-name message :direction :r)
@@ -24,10 +26,71 @@
       (rq-queue/push! client queue-name message :pattern :pending)
       (t/is (= message (rq-queue/pop! client queue-name :pattern :pending))))
 
-    ;; TODO
-    (t/testing "block" 
-      (rq-queue/bpop! client queue-name 10000 message)
-      (t/is (= message (rq-queue/pop! client queue-name message)))
-      )
+    (t/testing "bpop! left"
+      (while (not (nil? (rq-queue/bpop! client queue-name 1 {:direction :l}))))
+      (rq-queue/push! client queue-name message)
+      (let [popped-message (rq-queue/bpop! client queue-name 1 {:direction :l})]
+        (t/is (= message popped-message))
+        (t/is (= 0 (rq-queue/llen client queue-name)))))
+
+    (t/testing "bpop! right"
+      (while (not (nil? (rq-queue/bpop! client queue-name 1 {:direction :r}))))
+      (rq-queue/push! client queue-name message)
+      (let [popped-message (rq-queue/bpop! client queue-name 1 {:direction :r})]
+        (t/is (= message popped-message))
+        (t/is (= 0 (rq-queue/llen client queue-name)))))
+
+    (t/testing "lset"
+      (while (not (nil? (rq-queue/bpop! client queue-name 1 {:direction :l}))))
+      (rq-queue/push! client queue-name message)
+      (rq-queue/push! client queue-name another-message)
+      (rq-queue/lset client queue-name 0 another-message)
+      (t/is (= another-message (rq-queue/lindex client queue-name 0)))
+      (rq-queue/lset client queue-name 1 message)
+      (t/is (= message (rq-queue/lindex client queue-name 1))))
+
+    (t/testing "lrange"
+      (while (not (nil? (rq-queue/bpop! client queue-name 1 {:direction :l}))))
+      (rq-queue/push! client queue-name message)
+      (rq-queue/push! client queue-name another-message)
+      (t/is (= [message another-message] (rq-queue/lrange client queue-name 0 1))))
+
+    (t/testing "lindex"
+      (rq-queue/lset client queue-name 0 message)
+      (t/is (= message (rq-queue/lindex client queue-name 0))))
+
+    (t/testing "lrem"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/lrem client queue-name 1 message)
+      (t/is (= 0 (rq-queue/llen client queue-name))))
+
+    (t/testing "linsert"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/linsert client queue-name another-message message :pos :before)
+      (t/is (= another-message (rq-queue/lindex client queue-name 0))))
+
+    (t/testing "ltrim"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/push! client queue-name another-message)
+      (rq-queue/ltrim client queue-name 1 -1)
+      (t/is (= another-message (rq-queue/lindex client queue-name 0))))
+
+    (t/testing "rpoplpush"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/rpoplpush client queue-name another-queue-name)
+      (t/is (= 0 (rq-queue/llen client queue-name)))
+      (t/is (= message (rq-queue/pop! client another-queue-name :direction :l))))
+
+    (t/testing "brpoplpush"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/brpoplpush client queue-name another-queue-name 1)
+      (t/is (= 0 (rq-queue/llen client queue-name)))
+      (t/is (= message (rq-queue/pop! client another-queue-name :direction :l))))
+
+    (t/testing "lmove"
+      (rq-queue/push! client queue-name message)
+      (rq-queue/lmove client queue-name another-queue-name "LEFT" "RIGHT")
+      (t/is (= 0 (rq-queue/llen client queue-name)))
+      (t/is (= message (rq-queue/pop! client another-queue-name :direction :r))))
 
     (rq/close-client client)))
