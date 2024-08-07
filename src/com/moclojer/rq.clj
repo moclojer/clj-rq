47ff1dcd8592da53005cc355691cf5c7516af571
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-07 16:11:20 -0300

    feat: scratch method retrieval

--- src/com/moclojer/rq.clj
+++ src/com/moclojer/rq.clj
@@ -26,3 +26,41 @@
   "Disconnect and close redis client"
   ([] (close-client *redis-pool*))
   ([client] (.close @client)))
+
+(comment
+  ;; (import [java.util Arrays])
+  (require '[clojure.string :as str]
+           '[clojure.java.io :as io]
+           '[clojure.edn :as edn]
+           '[camel-snake-kebab.core :as csk])
+  (import [redis.clients.jedis JedisPooled])
+
+  (def allowlist
+    (-> (io/resource "command-allowlist.edn")
+        (slurp)
+        (edn/read-string)))
+
+  (->> (.getMethods JedisPooled)
+       (map
+        (fn [class]
+          {:name (csk/->kebab-case (.getName class))
+           :parameters (map
+                        #(identity
+                          {:type (-> (.. % getType getName)
+                                     (str/split #"\.")
+                                     (last)
+                                     (csk/->kebab-case))
+                           :name (csk/->kebab-case (.getName %))})
+                        (.getParameters class))}))
+       (filter #(contains? allowlist (:name %)))
+       (reduce
+        (fn [methods {:keys [name parameters]}]
+          (let [overload-count (count
+                                (filter
+                                 #(str/starts-with? (key %) name)
+                                 methods))
+                cur-overload-id (when (> overload-count 0) overload-count)]
+            (assoc methods (str name cur-overload-id) parameters)))
+        {}))
+  ;;
+  )
