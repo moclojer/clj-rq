d206efa25d9e242f2d36e140d8130b76f9676bd6
J0sueTM <teodoro.josue@protonmail.ch>
2024-09-04 17:28:38 -0300

    refactor: group similar tests into t/are

--- test/com/moclojer/rq/adapters_test.clj
+++ test/com/moclojer/rq/adapters_test.clj
@@ -5,15 +5,21 @@
    [com.moclojer.rq.adapters :as adapters]))
 
 (t/deftest pattern->str-test
-  [(t/is "my-queue" (adapters/pack-pattern :none "my-queue"))
-   (t/is "rq:my-queue" (adapters/pack-pattern :rq "my-queue"))
-   (t/is "rq:pubsub:my-queue" (adapters/pack-pattern :pubsub "my-queue"))
-   (t/is "rq:pubsub:pending:my-queue" (adapters/pack-pattern :pending "my-queue"))]
+  (t/are [expected pattern queue-name] (= expected
+                                          (adapters/pack-pattern
+                                           pattern queue-name))
+    "my-queue"                   :none    "my-queue"
+    "rq:my-queue"                :rq      "my-queue"
+    "rq:pubsub:my-queue"         :pubsub  "my-queue"
+    "rq:pubsub:pending:my-queue" :pending "my-queue")
 
-  [(t/is "my-queue" (adapters/unpack-pattern :none "my-queue"))
-   (t/is "my-queue" (adapters/unpack-pattern :rq "rq:my-queue"))
-   (t/is "my-queue" (adapters/unpack-pattern :pubsub "rq:pubsub:my-queue"))
-   (t/is "my-queue" (adapters/unpack-pattern :pending "rq:pubsub:pending:my-queue"))])
+  (t/are [expected pattern queue-name] (= expected
+                                          (adapters/unpack-pattern
+                                           pattern queue-name))
+    "my-queue" :none    "my-queue"
+    "my-queue" :rq      "rq:my-queue"
+    "my-queue" :pubsub  "rq:pubsub:my-queue"
+    "my-queue" :pending "rq:pubsub:pending:my-queue"))
 
 (t/deftest encode-test
   (t/testing "keyword encoders"
@@ -28,12 +34,11 @@
     (t/is (= "HELLO WORLD" (adapters/encode str/upper-case "hello world")))))
 
 (t/deftest decode-test
-  (t/testing "keyword decoders"
-    [(t/is (= "hello world" (adapters/decode :none "hello world")))
-     (t/is (= {:hello? true} (adapters/decode :edn "{:hello? true}")))
-     (t/is (= {:hello? true} (adapters/decode :json "{\"hello?\":true}")))
-     (t/is (= ["3" "true"] (adapters/decode :array (into-array ["3" "true"]))))
-     (t/is (= [3 true] (adapters/decode :edn-array (into-array ["3" "true"]))))
-     (t/is (= [{:hello? true}] (adapters/decode
-                                :json-array
-                                (into-array ["{\"hello?\":true}"]))))]))
+  (t/are [expected decoding value] (= expected
+                                      (adapters/decode decoding value))
+    "hello world"    :none "hello world"
+    {:hello? true}   :edn "{:hello? true}"
+    {:hello? true}   :json "{\"hello?\":true}"
+    ["3" "true"]     :array (into-array ["3" "true"])
+    [3 true]         :edn-array (into-array ["3" "true"])
+    [{:hello? true}] :json-array (into-array ["{\"hello?\":true}"])))
